d07e07387c21ab38114d02024cb6d13b4c48ba5c
Gabriel <biel.jaeger03@gmail.com>
2020-01-09 23:55:28 +0700

    OAuth sign in final

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,5 +5,5 @@ const userController = require('../controllers/userController')
 
 router.get('/', userController.getUsers)
 
-router.get('/google-sign-in', userController.signIn)
+router.post('/google-sign-in', userController.signIn)
 module.exports = router

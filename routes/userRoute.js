fcc162adc86f09cd965fe9c2cf8b9c9c7d1c84d9
Gabriel <biel.jaeger03@gmail.com>
2020-01-10 11:39:18 +0700

    fix routing gak ke commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,4 +6,8 @@ const userController = require('../controllers/userController')
 router.get('/', userController.getUsers)
 
 router.post('/google-sign-in', userController.signIn)
+
+router.get('/game', userController.startGame)
+
+router.post('/game', userController.checkAnswers)
 module.exports = router
